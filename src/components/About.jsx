065433c2d0814de15ad251c6731fdf1d452da46b
sagarreddy4142 <sagarreddy4142@gmail.com>
2024-07-29 15:15:11 -0500

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import '../App.css';
+import { services } from '../constants';
+import ButtonLink from './ButtonLink';
+import Footer from './Footer';
+
+const ServiceCard = ({ service }) => (
+  <div className='sm:w-[250px] w-full'>
+    <div
+      className='w-full green-pink-gradient p-[1px] rounded-[20px]'>
+      <div
+        className='rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        style={{ background: '#151030' }}>
+        <img
+          src={service.icon}
+          alt='some_icon'
+          className='w-16 h-16 object-contain'
+        />
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {service.title}
+        </h3>
+      </div>
+    </div>
+  </div>
+);
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='bg-black h-full w-full text-white sm:flex sm:justify-around about py-12 mt-8 overflow-x-hidden' id='about'>
+        <div className='flex flex-col justify-around'>
+          <div className='sm:px-16 px-2'>
+            <h2 className='text-4xl sm:text-5xl font-extrabold mt-2'>Introduction</h2>
+            <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>👨‍💻 Hello, I'm Vidyasagar, a proud member of the 2024 graduating class! 🎓 I am a dynamic and detail-oriented Software Developer with a robust background in both web development and backend solutions. I have a proven track record of enhancing application performance and user experience by implementing efficient design features using Next.js, React.js, and Spring Boot.
+
+I thrive in collaborative environments, working with cross-functional teams and employing Agile methodologies to deliver innovative solutions. My commitment to continuous learning drives me to leverage my technical skills to achieve impactful results in fast-paced settings.</p>
+            <ButtonLink
+              url='https://drive.google.com/file/d/1vbr0G48z9S7jbG4jSfWydlkBxab3fNaj/view?usp=sharing'
+              text='View Resume →'
+              padding={`p-3`}
+            />
+
+          </div>
+          <div className='mt-20 flex justify-center flex-wrap gap-7'>
+            {services.map((service) => (
+              <ServiceCard service={service} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default About;
