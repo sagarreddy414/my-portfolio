ad7cb9b60a0109b44b21abf27dbf60ebf2e751ad
sagarreddy4142 <sagarreddy4142@gmail.com>
2024-07-29 15:55:53 -0500

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter ,Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
@@ -11,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/experience' element={<Experience/>}/>
+        <Route path='*' element={<Navigate to="/" replace />} />
       </Routes>
       <Footer/>
     </BrowserRouter>
