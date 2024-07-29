67d3ea5a370fa81931f34d15af6a226ea644ece0
sagarreddy4142 <sagarreddy4142@gmail.com>
2024-07-29 16:04:40 -0500

    changed

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ import Projects from './components/Projects';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-      <Navbar/>
+      <Home/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Navbar/>}/>
         <Route path='/experience' element={<Experience/>}/>
         <Route path='/projects' element={<Projects/>}/>
         <Route path='/about' element={<About/>}/>
